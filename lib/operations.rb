958ba0548ea45a5760f9c6c1b053b8f439cc6eb5
Bethany Holley <chocolatechipfaced@gmail.com>
2019-10-10 23:10:19 +0000

    Automatically backed up by Learn

--- lib/operations.rb
+++ lib/operations.rb
@@ -1,11 +1,16 @@
 def unsafe?(speed)
-  speed > 60 ? TRUE : FALSE
+  if speed < 40 || speed > 60
+    return TRUE
+    
+  else 
+    return FALSE
+  end
 end
 
 
 
 def not_safe?(speed)
-	
+	speed > 60 ? TRUE : FALSE
 end
 	
 
